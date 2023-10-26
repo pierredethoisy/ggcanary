47ccb9cdc8ac1418fccc9d27279df74bfe1125ef
Pierre de Thoisy <113915383+pierredethoisy@users.noreply.github.com>
2023-10-26 11:59:28 +0200

    Create python_test.py

---
+++ python_test.py
@@ -0,0 +1,7 @@
+def main():
+    name = input("Please enter your name: ")
+    print(f"Hello, {name}! Welcome to the world of Python!")
+    password= "4SUY8QLy813352a-8cf“
+
+if __name__ == "__main__":
+    main()
